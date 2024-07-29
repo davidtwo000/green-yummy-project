995d76ba0a905010ca38515ef568136d6c175104
davidtwo000 <davidtwo3208@gmail.com>
2024-07-29 09:47:46 +0900

    .

--- green-yummy/src/main/java/com/example/demo/restController/UserRestController.java
+++ green-yummy/src/main/java/com/example/demo/restController/UserRestController.java
@@ -1,4 +1,4 @@
-package com.example.demo.controller;
+package com.example.demo.restController;
 
 import com.example.demo.domain.User;
 import com.example.demo.service.UserService;
---
+++ green-yummy/src/main/java/com/example/demo/service/userService/tmp.java
@@ -0,0 +1,5 @@
+package com.example.demo.service.userService;
+
+public class tmp {
+
+}
