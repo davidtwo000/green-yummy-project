a24f272243cd4e6ec645c5ac22d0ffb94c494aaa
davidtwo000 <davidtwo3208@gmail.com>
2024-07-29 10:29:39 +0900

    .

---
+++ green-yummy/src/main/java/com/example/demo/repository/publicRepository/tmp.java
@@ -0,0 +1,5 @@
+package com.example.demo.repository.publicRepository;
+
+public class tmp {
+
+}
--- green-yummy/src/main/java/com/example/demo/restController/UserRestController.java
+++ green-yummy/src/main/java/com/example/demo/restController/UserRestController.java
@@ -1,21 +1,11 @@
 package com.example.demo.restController;
 
-import com.example.demo.domain.User;
-import com.example.demo.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 public class UserRestController {
 
-    @Autowired
-    private UserService userService;
-
-    @GetMapping("/users")
-    public List<User> getUsers() {
-        return userService.getAllUsers();
-    }
+	
 }
