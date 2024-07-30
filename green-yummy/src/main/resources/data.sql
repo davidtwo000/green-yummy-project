4de35757976484acb24de5da8a2a41045ef11d14
davidtwo000 <davidtwo3208@gmail.com>
2024-07-30 10:23:19 +0900

    Create data.sql

---
+++ green-yummy/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+
+INSERT INTO users (name, email, password, id) VALUES ('john_doe', 'john@example.com', '12345', 'johoho');
+INSERT INTO users (name, email, password, id) VALUES ('jooheu', 'john@gmail.com', '12345' ,'joo');
+
+SELECT * FROM users;
