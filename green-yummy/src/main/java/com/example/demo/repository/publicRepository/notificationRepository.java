3f72c49acf0bd6f480154470cb4fdedcb64351a4
davidtwo000 <davidtwo3208@gmail.com>
2024-07-30 14:29:17 +0900

    .

---
+++ green-yummy/src/main/java/com/example/demo/repository/publicRepository/notificationRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository.publicRepository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface notificationRepository extends JpaRepository<Notification, Integer>{
+
+}
