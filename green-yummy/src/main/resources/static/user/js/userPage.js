cc5b9cb9a294a2a4da63825243a54da02c0e25a2
abcbinch <162808088+abcbinch@users.noreply.github.com>
2024-08-06 09:34:21 +0900

    usercontroller userpage
    storecollection과 reviewlist를
    userpage에 통합.
    usercontroller 경로 변경

--- green-yummy/src/main/resources/static/user/js/userPage.js
+++ green-yummy/src/main/resources/static/user/js/userPage.js
@@ -3,9 +3,9 @@ let userOut = document.querySelector(".modalContainer2");
 let modalCancel = document.querySelectorAll(".cancel");
 
 let storeTab = document.querySelector(".storeTab");
-let storePage = document.querySelector(".stores");
+let storePage = document.querySelector("#tab1");
 let reviewTab = document.querySelector(".reviewTab");
-let reviewPage = document.querySelector(".reviews");
+let reviewPage = document.querySelector("#tab2");
 
 
 //회원 정보란의 버튼들. 각각 회원정보수정, 로그아웃, 회원탈퇴 버튼.
@@ -30,8 +30,10 @@ function userBye(){
 //탭 메뉴. 나의 맛집, 나의 리뷰.
 //클릭을 한 쪽에 클래스를 주려면?
 
-reviewTab.onclick = () => {
+
+reviewTab.onclick = (e) => {
 	if(!reviewTab.classList.contains('youHere')){
+		e.preventDefault();
 		reviewTab.classList.add('youHere');
 		reviewPage.classList.remove('hidden');
 		storeTab.classList.remove('youHere');
@@ -40,8 +42,9 @@ reviewTab.onclick = () => {
 		return true;
 	}
 }
-storeTab.onclick = () => {
+storeTab.onclick = (e) => {
 	if(!storeTab.classList.contains('youHere')){
+		e.preventDefault();
 		storeTab.classList.add('youHere');
 		storePage.classList.remove('hidden');
 		reviewTab.classList.remove('youHere');
