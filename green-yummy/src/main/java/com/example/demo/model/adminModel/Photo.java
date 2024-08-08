f94c9d28979a9e2019a21fe57b9b6c495f2a6333
davidtwo000 <davidtwo3208@gmail.com>
2024-08-08 10:28:06 +0900

    엔티티 수정

--- green-yummy/src/main/java/com/example/demo/model/adminModel/Photo.java
+++ green-yummy/src/main/java/com/example/demo/model/adminModel/Photo.java
@@ -2,6 +2,7 @@ package com.example.demo.model.adminModel;
 
 import com.example.demo.model.userModel.Review;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,11 +13,12 @@ import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
-@Table(name = "PHOTOS")
+@Table(name = "SHOP_PHOTOS")
 @Data
 public class Photo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "SHOP_PHOTOS_ID")
     private Integer photoId;
 
     @ManyToOne
