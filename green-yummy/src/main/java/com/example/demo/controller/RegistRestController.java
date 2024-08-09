9ac03776ce1eab5208f7697bbd79fae0201ebe9c
davidtwo000 <davidtwo3208@gmail.com>
2024-08-09 13:15:41 +0900

    아이디비번 찾기 기능

--- green-yummy/src/main/java/com/example/demo/controller/RegistRestController.java
+++ green-yummy/src/main/java/com/example/demo/controller/RegistRestController.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.model.userModel.User;
+import com.example.demo.service.publicService.FindIdPasswordService;
 import com.example.demo.service.publicService.UserRegistService;
 
 @RestController
@@ -30,6 +32,27 @@ public class RegistRestController {
         return ResponseEntity.ok(isNicknameAvailable);
     }
 	
+    @Autowired
+    private FindIdPasswordService userService;
     
+    @PostMapping("/findId")
+    public ResponseEntity<String> findId(@RequestParam(name = "idPhone") String idPhone, 
+                         @RequestParam(name = "idEmail") String idEmail) {
+        Optional<String> userId = userService.findIdByPhoneAndEmail(idPhone, idEmail);
+
+        String message = userId.map(id -> "당신의 아이디는 :  " + id + "  입니다.")
+                .orElse("정보와 일치하는 아이디가 없습니다.");
+        return ResponseEntity.ok(message);
+    }
+
+    @PostMapping("/resetPassword")
+    public ResponseEntity<String> resetPassword(@RequestParam(name = "pswdEmail") String pswdEmail, 
+    					@RequestParam(name = "useId") String useId) {
+        Optional<String> newPassword = userService.resetPasswordByEmailAndId(useId, pswdEmail);
+
+        String message = newPassword.map(password -> "임시 비밀번호는  " + password + "  입니다.\n로그인 후 정보 수정에서 비밀번호를 재설정하세요.")
+                .orElse("정보와 일치하는 아이디가 없습니다.");
+        return ResponseEntity.ok(message);
+    }
     
 }
