a8a9ee185b8a7b43119f93462d54b1e1d6a88c8a
abcbinch <162808088+abcbinch@users.noreply.github.com>
2024-08-13 16:27:55 +0900

    .

--- green-yummy/src/main/resources/static/index/js/indexMenubar.js
+++ green-yummy/src/main/resources/static/index/js/indexMenubar.js
@@ -1,23 +1,28 @@
 
 function search(data) {
 
-    const lists = document.querySelectorAll('.searchByArea, .searchByType, .searchByRating');
+    const lists = document.querySelectorAll('.list-show ul');
+	const sublists = document.querySelectorAll('.list-show ul li');
 
     lists.forEach(list => list.style.display = 'none');
+	
 
+	sublists[0].classList.add("listSelect");
     
 
     if (data === 'area') {
-
+		const sublists = document.querySelectorAll('.list-show ul li');
+		
         document.getElementById('searchByArea').style.display = 'flex';
+		sublists[0].classList.add("listSelect");
+		sublists[1].classList.remove("listSelect");
 
     } else if (data === 'type') {
+		const sublists = document.querySelectorAll('.list-show ul li');
 
         document.getElementById('searchByType').style.display = 'flex';
-
-    } else if (data === 'rating') {
-
-        document.getElementById('searchByRating').style.display = 'flex';
+		sublists[0].classList.remove("listSelect");
+		sublists[1].classList.add("listSelect");
 
     }
 
